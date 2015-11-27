9501c099f809946ef12334e23e80a7a9d53f11bd
Cesar Felce Peroza <binpath@FelceiMac.local>
2015-11-27 17:33:30 -0300

    3 Ayudas de búsquedas

--- WebContent/view/Master.controller.js
+++ WebContent/view/Master.controller.js
@@ -34,5 +34,110 @@ sap.ui.controller("view.Master", {
 //	onExit: function() {
 //
 //	}
+	
+	/**
+	* Called Search Help Positions
+	* @memberOf view.Master
+	*/	
+	
+	handleValueHelpPositions : function (oController) {
+	    this.inputId = oController.oSource.sId;
+	    // create value help dialog
+	    if (!this._valueHelpDialogPositions) {
+	      this._valueHelpDialogPositions = sap.ui.xmlfragment("DialogPositions","view.DialogPositions",this);
+	      this.getView().addDependent(this._valueHelpDialogPositions);
+	    }
+
+	    // open value help dialog
+	    this._valueHelpDialogPositions.open();
+	  },
+	  
+	  _handleValueHelpSearchPositions : function (evt) {
+		    var sValue = evt.getParameter("value");
+		    var oFilter = new sap.ui.model.Filter(
+		      "Plstx",
+		      sap.ui.model.FilterOperator.Contains, sValue
+		    );
+		    evt.getSource().getBinding("items").filter([oFilter]);
+		  },
+
+		  _handleValueHelpClosePositions : function (evt) {
+		    var oSelectedItem = evt.getParameter("selectedItem");
+		    if (oSelectedItem) {
+		      var productInput = this.getView().byId(this.inputId);
+		      productInput.setValue(oSelectedItem.getTitle());
+		    }
+		    evt.getSource().getBinding("items").filter([]);
+		  },
+
+		/**
+		* Called Search Help PersonnelArea
+		* @memberOf view.Master
+		*/	
+			
+		handleValueHelpPersonnelArea : function (oController) {
+			    this.inputId = oController.oSource.sId;
+			    // create value help dialog
+			    if (!this._valueHelpDialogPersonnelArea) {
+			      this._valueHelpDialogPersonnelArea = sap.ui.xmlfragment("DialogPersonnelArea","view.DialogPersonnelArea",this);
+			      this.getView().addDependent(this._valueHelpDialogPersonnelArea);
+			    }
+
+			    // open value help dialog
+			    this._valueHelpDialogPersonnelArea.open();
+			  },
+			  
+			  _handleValueHelpSearchPersonnelArea : function (evt) {
+				    var sValue = evt.getParameter("value");
+				    var oFilter = new sap.ui.model.Filter(
+				      "Name1",
+				      sap.ui.model.FilterOperator.Contains, sValue
+				    );
+				    evt.getSource().getBinding("items").filter([oFilter]);
+				  },
+
+				  _handleValueHelpClosePersonnelArea : function (evt) {
+				    var oSelectedItem = evt.getParameter("selectedItem");
+				    if (oSelectedItem) {
+				      var productInput = this.getView().byId(this.inputId);
+				      productInput.setValue(oSelectedItem.getTitle());
+				    }
+				    evt.getSource().getBinding("items").filter([]);
+				  },
+				  
+		/**
+		* Called Search Help PersonnelGroup
+		* @memberOf view.Master
+		* 
+		* */	
+		handleValueHelpPersonnelGroup : function (oController) {
+			this.inputId = oController.oSource.sId;
+			// create value help dialog
+			if (!this._valueHelpDialogPersonnelGroup) {
+				this._valueHelpDialogPersonnelGroup = sap.ui.xmlfragment("DialogPersonnelGroup","view.DialogPersonnelGroup",this);
+				this.getView().addDependent(this._valueHelpDialogPersonnelGroup);
+			}
+
+			// open value help dialog
+			this._valueHelpDialogPersonnelGroup.open();
+			},
+					  
+		_handleValueHelpSearchPersonnelGroup : function (evt) {
+			var sValue = evt.getParameter("value");
+			var oFilter = new sap.ui.model.Filter(
+					"Ptext",
+					sap.ui.model.FilterOperator.Contains, sValue
+			);
+			evt.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		_handleValueHelpClosePersonnelGroup : function (evt) {
+			var oSelectedItem = evt.getParameter("selectedItem");
+			if (oSelectedItem) {
+				var productInput = this.getView().byId(this.inputId);
+				productInput.setValue(oSelectedItem.getTitle());
+			}
+			evt.getSource().getBinding("items").filter([]);
+		}		  
 
 });
